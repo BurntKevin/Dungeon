a735679877d2ae7db62edae94058b7858320a9fc
Kevin <kevin.chu1@student.unsw.edu.au>
2020-07-20 17:18:25 +1000

    Cleaned up Boulder

--- src/unsw/dungeon/Boulder.java
+++ src/unsw/dungeon/Boulder.java
@@ -1,35 +1,42 @@
 package unsw.dungeon;
 
+/**
+ * Boulder which is placed around the map
+ */
 public class Boulder extends Entity {
-
+    /**
+     * Initalises a boulder onto the board
+     * @param x x Coordinate
+     * @param y y Coordinate
+     */
     public Boulder(int x, int y) {
         super(x, y);
     }
 
     /**
-     * 
-     * @param next
-     * @param moveDir
-     * @return
+     * Attempts to push an object
+     * @param next Entity to be pushed
+     * @param moveDir Direction which the entity is pushed
+     * @return Whether the push was completed
      */
     public boolean attemptPush(Entity next, String moveDir) {
-        if (next instanceof Wall || next instanceof Boulder 
-            || next instanceof Door || next instanceof Door) {
+        // Checking whether the push can be completed
+        if (next instanceof Wall || next instanceof Boulder || next instanceof Door) {
             return false;
         }
         
-        // update coordinates if can be moved
+        // Successfully pushing the entity
         if (moveDir.equals("Left")) {
-            x().set(getX()-1);
+            x().set(getX() - 1);
         }
         else if (moveDir.equals("Right")) {
-            x().set(getX()+1);
+            x().set(getX() + 1);
         }
         else if (moveDir.equals("Up")) {
-            y().set(getY()-1);
+            y().set(getY() - 1);
         }
         else if (moveDir.equals("Down")) {
-            y().set(getY()+1);
+            y().set(getY() + 1);
         }
         return true;
     }
