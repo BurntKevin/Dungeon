69e8028a57f9e437b3609f9cc04131c4edee2084
TIana Tsang Ung <z5260040@ad.unsw.edu.au>
2020-07-28 17:15:52 +1000

    Added Title Screen file outline

---
+++ src/unsw/dungeon/TitleScreen.java
@@ -0,0 +1,39 @@
+package unsw.dungeon;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TitleScreen {
+    private Stage stage;
+    private String title;
+    private TitleController controller;
+
+    private Scene scene;
+
+    public TitleScreen(Stage stage) throws IOException {
+        this.stage = stage;
+        title = "Title";
+
+        controller = new TitleController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("title.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
+        root.requestFocus();
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    public void start() {
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public TitleController getController() {
+        return controller;
+    }
+
+}
